0523a7bb85e59712337f4fdfc25ab509793b59fe
varMarat <56462327+varMarat@users.noreply.github.com>
2019-10-25 17:56:10 +0600

    третий блок "Travel"

--- app/js/main.js
+++ app/js/main.js
@@ -30,5 +30,13 @@ $(function(){
 			focusOnSelect: true,
 	})
 
+	$('.travel__slider').slick({
+		infinite: true,
+		fade: true,
+		prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+		nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+		asNavFor: '.slider-dotshead',
+	})
+
 });
 
